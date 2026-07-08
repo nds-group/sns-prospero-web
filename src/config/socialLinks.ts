eac4257af41c8633864b248858aa3e31321345df
Josue M. Aguilar <josmiguel92@gmail.com>
2026-07-08 17:11:45 +0200

    feat(social-links): add social media links configuration and update footer to display links

---
+++ src/config/socialLinks.ts
@@ -0,0 +1,32 @@
+export const socialLinks = [
+	{
+		name: 'LinkedIn',
+		href: 'https://www.linkedin.com/in/sns-prospero/',
+		icon: 'business_center',
+		label: 'PROSPERO on LinkedIn',
+	},
+	{
+		name: 'Twitter/X',
+		href: 'https://x.com/sns_prospero',
+		icon: 'alternate_email',
+		label: 'PROSPERO on Twitter/X',
+	},
+	{
+		name: 'YouTube',
+		href: 'https://www.youtube.com/@sns-prospero',
+		icon: 'smart_display',
+		label: 'PROSPERO on YouTube',
+	},
+	{
+		name: 'Instagram',
+		href: 'https://www.instagram.com/prospero_sns/',
+		icon: 'photo_camera',
+		label: 'PROSPERO on Instagram',
+	},
+	{
+		name: 'Bluesky',
+		href: 'https://bsky.app/profile/sns-prospero.bsky.social',
+		icon: 'cloud',
+		label: 'PROSPERO on Bluesky',
+	},
+] as const;
